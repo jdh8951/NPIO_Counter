0d5c320a1ed035e1b304372b1076e601c8ea3465
jdh8951 <83170563+jdh8951@users.noreply.github.com>
2021-06-17 16:29:59 +0900

    계수기테스트
    계수기테스트

---
+++ thread.h
@@ -0,0 +1,27 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+// #include "mainwindow.h"
+// #include <QProcess>
+// #include <QThread>
+//
+// class strThread : public QThread
+// {
+//     Q_OBJECT
+// public:
+//     explicit strThread(QObject *parent = 0);
+//     QProcess *Process;
+//
+// private:
+//     void run();     //qthread 실행
+//
+// signals:
+//     void Send_Signal(QString Server_Current_Info);
+//
+//
+// public slots:
+//
+// };
+
+
+#endif // THREAD_H
